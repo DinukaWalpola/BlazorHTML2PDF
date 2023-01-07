2ea735abee8a1fe1c7c1ae4e9c8a262c3779e794
DinukaWalpola <91844262+DinukaWalpola@users.noreply.github.com>
2023-01-07 09:21:11 +0530

    Initial Commit

---
+++ Html2PDF/Html2PDF/wwwroot/js/pdfDownloader.js
@@ -0,0 +1,32 @@
+﻿//function generatePDF(name, email, phone) {
+
+//    var pdf = new jsPDF({
+//        orientation: 'p',
+//        unit: 'mm',
+//        format: 'a4',
+//        putOnlyUsedFonts: true
+//    });
+//    pdf.text(name, 10, 10);
+//    pdf.text(email, 10, 10);
+//    pdf.text(phone, 10, 10);
+//    // pdf.text("published on APITemplate.io", 20, 30);
+//    pdf.addPage();
+//    pdf.text(20, 20, 'The second page');
+//    pdf.save('jsPDF_2Pages.pdf');
+
+//}
+
+
+function generatePDF() {
+	console.log(document.getElementById('main-container'))
+	const element = document.getElementById('main-container');
+	var opt = {
+		margin: 1,
+		filename: 'html2pdf_example.pdf',
+		image: { type: 'jpeg', quality: 0.98 },
+		html2canvas: { scale: 2 },
+		jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+	};
+	// Choose the element that our invoice is rendered in.
+	html2pdf().set(opt).from(element).save();
+}
